5fff3f6455c31276c450ba5c204e49922bd1a69b
dev <vhzmepqm@kakao.com>
2022-05-12 16:48:11 +0900

    ommt

--- sbmember/src/main/java/com/example/demo/controller/SessionCheckInter.java
+++ sbmember/src/main/java/com/example/demo/controller/SessionCheckInter.java
@@ -16,13 +16,13 @@ public class SessionCheckInter extends HandlerInterceptorAdapter {
 	// 1.Controller에서 요청(*.do)을 받기 전에  preHandle()가 호출되어 가로채는 역할로 사용
 	// 2.로그인 하지않고(세션이 없으면) 요청하면 로그인 폼으로 이동 하도록 해주는 역할
 	public boolean preHandle(HttpServletRequest request, 
-			HttpServletResponse response, Object handler) throws Exception {
+			HttpServletResponse response, Object handler) throws  Exception {
 		HttpSession session = request.getSession();
 		String id = (String)session.getAttribute("id");
 		if (id == null || id.equals(""))  {		
 			response.sendRedirect("member_login.do");	// 세션이 없으면 로그인 폼으로 이동
 			return false;
 		}
-		return true;
+		return true; 
 	}
 }
